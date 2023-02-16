986ef109c1a1b9ee94b1bc60e30589ce7554e163
Davis Lamenha Sampaio <davislamenha@gmail.com>
2023-02-16 11:37:43 -0300

    adicionar imagem na galeria

--- main.js
+++ main.js
@@ -9,4 +9,13 @@ $(document).ready(() => {
 
 $('form').on('submit', (e) => {
   e.preventDefault();
+  const enderecoDaNovaImagem = $('#endereco-imagem-nova').val();
+  const novoItem = $('<li style="display: none"></li>');
+  $(`<img src="${enderecoDaNovaImagem}" />`).appendTo(novoItem);
+  $(
+    `<div class="overlay-img-link"><a href="${enderecoDaNovaImagem}" target="_blank" title="Ver imagem em tamanho real">Ver imagem em tamanho real</a>`,
+  ).appendTo(novoItem);
+  $(novoItem).appendTo('ul');
+  $(novoItem).fadeIn(800);
+  $('#endereco-imagem-nova').val('');
 });
